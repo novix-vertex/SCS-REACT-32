bf22120a09cb8853e899fd8867a0af6672dfa823
chirag jain <jchirag0610@gmail.com>
2026-05-02 22:37:28 +0530

    home page ui added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,11 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className=" w-full bg-[#F5EDE2] flex flex-col justify-center py-20 align-center gap-4 p-10 relative">
+      <h1 className="font-bold text-8xl text-[#FCB505] w-[50%]">Stylish Product Gallery</h1>
+      <h4 className="font-[400] text-2xl text-[#30302b] w-[50%]">Specially choosen from various premium stores</h4>
+      <h5 className="font-[600] text-l text-[#ddc396] w-[50%]">Discover a curated collection of stylish products designed to elevate your everyday living. Our gallery brings together modern aesthetics, premium quality, and timeless appeal in one seamless experience. From elegant essentials to bold statement pieces, each item reflects thoughtful craftsmanship and contemporary design. Explore effortlessly through beautifully organized categories that suit every taste and lifestyle. Whether you’re refreshing your space or finding something unique, our selection is made to inspire. Experience the perfect blend of function and style, where every product tells a story and transforms your surroundings into something truly exceptional.</h5>
+      <img src="https://i.pinimg.com/1200x/77/e7/91/77e7913b756c1f5627b35b9ba4c42a5b.jpg" alt="" className="absolute bottom-0 right-0 h-125 w-125 aspect-square " />
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav className="bg-red-700 py-2 px-2 text-white flex justify-center items-center gap-4">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/contacts">Contact Us</Link>
+
+        <nav className="h-16 w-full text-[#F5EDE2] bg-[#FCB505] flex justify-between items-center px-10">
+            <h2 className="font-extrabold text-3xl">DesignerGallery</h2>
+            <menu className="font-semibold text-2xl flex justify-center items-center gap-4">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/contacts">Contact Us</Link>
+            </menu>
         </nav>
-        
+
     )
 }
 
